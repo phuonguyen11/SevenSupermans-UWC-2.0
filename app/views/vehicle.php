7f65282b7d3b804761499d729e5417a811fd7ef7
phuonguyen11 <phuonguyen11112002@gmail.com>
2022-12-15 01:54:39 +0700

    change footer

--- app/views/vehicle.php
+++ app/views/vehicle.php
@@ -357,7 +357,7 @@
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>Copyright © Your Website 2022</span>
+                            <span>Copyright © UWC 2.0</span>
                         </div>
                     </div>
                 </footer>
